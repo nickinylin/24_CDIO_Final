bc6e1103ea06c2e6e73ec3c8e6920ad3b2a53c8c
Mathias Kvisgaard <mathias@kvisgaard.info>
2016-01-05 15:12:17 +0100

    Rettede nogle småting

--- 24_CDIO_Final/src/game/CardsDeck.java
+++ 24_CDIO_Final/src/game/CardsDeck.java
@@ -14,28 +14,28 @@ public CardsDeck(){
 	deck = new Cards[]{
 
 			new CardsTransaction("Modtag udbytte af Deres aktier, modtag 1000kr", 1000),
-			new CardsTransaction("Modtag udbytte af Deres aktier, modtag 1000kr.", 1000),
-			new CardsTransaction("De har vundet i Klasselotteriet, modtag 500kr.", 500),
-			new CardsTransaction("De har vundet i Klasselotteriet, modtag 500kr.", 500),
-			new CardsTransaction("Deres præmieobligation er udtrukket. De modtager 1000kr.", 1000),
-			new CardsTransaction("Deres præmieobligation er udtrukket. De modtager 1000kr.", 1000),
-			new CardsTransaction("Værdien af egen avl fra nyttehaven udgør 200kr, som De modtager af banken.", 200),
-			new CardsTransaction("Kommunen har eftergivet et kvartals skat. Hæv i banken 3000kr.", 3000),
-			new CardsTransaction("De havde en række med elleve rigtige i tipning. Modtag 1000kr.", 1000),
-			new CardsTransaction("Grundet dyrtiden har de fået gageforhøjelse. Modtag 1000kr.", 1000),
-			new CardsTransaction("De har solgt nogle gamle møbler på auktion, og modtager 1000kr.", 1000),
+			new CardsTransaction("Modtag udbytte af Deres aktier, modtag 1000kr", 1000),
+			new CardsTransaction("De har vundet i Klasselotteriet, modtag 500kr", 500),
+			new CardsTransaction("De har vundet i Klasselotteriet, modtag 500kr", 500),
+			new CardsTransaction("Deres præmieobligation er udtrukket. De modtager 1000kr", 1000),
+			new CardsTransaction("Deres præmieobligation er udtrukket. De modtager 1000kr", 1000),
+			new CardsTransaction("Værdien af egen avl fra nyttehaven udgør 200kr, som De modtager af banken", 200),
+			new CardsTransaction("Kommunen har eftergivet et kvartals skat. Hæv i banken 3000kr", 3000),
+			new CardsTransaction("De havde en række med elleve rigtige i tipning. Modtag 1000kr", 1000),
+			new CardsTransaction("Grundet dyrtiden har de fået gageforhøjelse. Modtag 1000kr", 1000),
+			new CardsTransaction("De har solgt nogle gamle møbler på auktion, og modtager 1000kr", 1000),
 			new CardsTransaction("De modtager deres aktieudbytte. Modtag 1000kr af banken", 1000),
 			
-			new CardsTransaction("Betal deres bilforsikring.", -1000),
-			new CardsTransaction("De har været en tur i udlandet og har haft for mange cigaretter med hjem. Betal told 200kr.", -200),
-			new CardsTransaction("De har fået en parkeringsbøde. Betal 200kr.", -200),
-			new CardsTransaction("Betal 200kr for levering af 2 kasser øl.", -200),
-			new CardsTransaction("De har kørt frem for fuldt stop, betal 1000kr i bøde.", -1000),
-			new CardsTransaction("De har modtaget Deres tandlægeregning, betal 2000kr.", -2000),
-			new CardsTransaction("De har købt 4 nye dæk til Deres vogn, betal 1000kr.", -1000),
-			new CardsTransaction("Betal 3000kr for reparation af Deres vogn.", -3000),
-			new CardsTransaction("Betal 3000kr for reparation af Deres vogn.", -3000),
-			new CardsTransaction("Betal 300kr for vognvask og smøring.", -300),
+			new CardsTransaction("Betal deres bilforsikring", -1000),
+			new CardsTransaction("De har været en tur i udlandet og har haft for mange cigaretter med hjem. Betal told 200kr", -200),
+			new CardsTransaction("De har fået en parkeringsbøde. Betal 200kr", -200),
+			new CardsTransaction("Betal 200kr for levering af 2 kasser øl", -200),
+			new CardsTransaction("De har kørt frem for fuldt stop, betal 1000kr i bøde", -1000),
+			new CardsTransaction("De har modtaget Deres tandlægeregning, betal 2000kr", -2000),
+			new CardsTransaction("De har købt 4 nye dæk til Deres vogn, betal 1000kr", -1000),
+			new CardsTransaction("Betal 3000kr for reparation af Deres vogn", -3000),
+			new CardsTransaction("Betal 3000kr for reparation af Deres vogn", -3000),
+			new CardsTransaction("Betal 300kr for vognvask og smøring", -300),
 			
 			// Er slashed ud indtil vi har fundet en løsning
 //			new CardsTransaction("Ejendomsskatterne er steget. Ekstraudgifterne er: 800kr pr. hus. 2300kr pr. hotel.", -800*amountOfHouses, 2300*amountOfHotels),
@@ -46,7 +46,8 @@ public CardsDeck(){
 //			new CardsTransaction("De modtager Matador-legatet for værdigt trængende på kr. 40.000. Ved værdigt trængende forstås, at Deres formue, dvs. Deres kantante penge + skøder + bygninger, ikke overstiger kr. 15.000", if(formue<15000){
 //				formue = formue+40000)}
 //			new GetOutofJail(),
-			new CardsMoveto("Ryk 3 felter frem", +3),
+			new CardsMoveto("Ryk 3 felter frem", 3),
+			new CardsMoveto("Ryk 3 felter frem", 3),
 			new CardsMoveto("Ryk 3 felter tilbage", -3),
 			new CardsMoveto("Ryk 3 felter tilbage", -3),
 			
@@ -62,12 +63,14 @@ public CardsDeck(){
 			new CardsMoveto("Ryk frem til Frederiksberg Allé. Hvis De passerer START, indkassér da kr. 4000", "Frederiksberg Allé"),
 			new CardsMoveto("Tag ind på rådhuspladsen", "Rådhuspladsen"),
 			
+			new CardsMoveto("Tag med Mols-linien. Flyt brikken frem, og hvis de passerer START, indkassér da kr. 4000", "Mols-linien")
+			
 //			new CardsMoveto("Ryk brikken frem til det nærmeste rederi og betal ejeren to gange den leje, han ellers er berretiget til. Hvis selskabet ikke ejes af nogen, kan De købe det af banken", nærmeste rederi),
 //			new CardsMoveto("Ryk brikken frem til det nærmeste rederi og betal ejeren to gange den leje, han ellers er berretiget til. Hvis selskabet ikke ejes af nogen, kan De købe det af banken", nærmeste rederi),
 //			new CardsMoveto("Tag med den nærmeste færge. Flyt brikken frem, og hvis De passerer START indkassér da kr. 4000", nærmeste færge),
-			new CardsMoveto("Tag med Mols-linien. Flyt brikken frem, og hvis de passerer START, indkassér da kr. 4000", "Mols-linien"),
 //			new GetOutofJail()
 	};
+	
 	discard = new Cards[deck.length];
 	currentdecksize=deck.length;
 	discardsize=0;
