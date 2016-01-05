db81409707947141971b534b8b17392831592685
Nicki N <nickinylin@hotmail.com>
2016-01-05 10:23:18 +0100

    Opdateret en variabler i Territory

--- 24_CDIO_Final/src/game/Territory.java
+++ 24_CDIO_Final/src/game/Territory.java
@@ -8,7 +8,7 @@ package game;
 
 public class Territory extends Ownable {
     
-    protected String fieldname;
+    private String fieldname;
     private int fieldPriceBuilding;
     private int fieldrent;
     private int fieldrenthouse1;
