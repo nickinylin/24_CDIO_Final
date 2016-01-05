7a7e0fc30baf5ca3aa00a44498cd4af554779a4e
pc <pc@10.16.100.219>
2016-01-05 09:17:00 +0100

    GUI tilføjet og felterne tilpasset

--- 24_CDIO_Final/src/game/Field.java
+++ 24_CDIO_Final/src/game/Field.java
@@ -6,6 +6,13 @@
 
 package game;
 
+import java.awt.Color;
+
+import desktop_fields.Brewery;
+import desktop_fields.Shipping;
+import desktop_fields.Street;
+import desktop_resources.GUI;
+
 public abstract class Field {
 
 	protected String fieldname;
@@ -19,47 +26,48 @@ public abstract class Field {
 
 		field = new Field[] { 
 				new Refuge ("Start",4000),
-				new Territory ("Rødovrevej",1200,1000,50,250,750,2250,4000,6000),
+				new Territory ("Rødovrevej",1,1200,1000,50,250,750,2250,4000,6000),
 				new Luck ("Prøv lykken"),
-				new Territory ("Hvidovrevej",1200,1000,50,250,750,2250,4000,6000),
-				new Tax ("Betal indkomst skat",4000, "10% af samlede værdier"),
+				new Territory ("Hvidovrevej",1,1200,1000,50,250,750,2250,4000,6000),
+				new Tax ("Betal indkomst skat",4000, "special"),
 				new Fleet ("Scanlines! Helsingør - Helsingborg"),
-				new Territory ("Roskildevej", 2000, 1000,100,600,1800,5400,8000,11000),
+				new Territory ("Roskildevej",2, 2000, 1000,100,600,1800,5400,8000,11000),
 				new Luck ("Prøv lykken"),
-				new Territory ("Valby Langgade", 2000, 1000,100,600,1800,5400,8000,11000),
-				new Territory ("Allegade", 2400, 1000,150,800,2000,6000,9000,12000),
+				new Territory ("Valby Langgade", 2, 2000, 1000,100,600,1800,5400,8000,11000),
+				new Territory ("Allegade", 2, 2400, 1000,150,800,2000,6000,9000,12000),
 				new Empty ("På besøg i fængsel"),
-				new Territory ("Frederiksberg Alle",2800,2000,200,1000,3000,9000,12500,15000),
+				new Territory ("Frederiksberg Alle", 3, 2800,2000,200,1000,3000,9000,12500,15000),
 				new Labor ("Tuborg Squash"),
-				new Territory ("Bulowsvej",2800,2000,200,1000,3000,9000,12500,15000),
-				new Territory ("Gl. Kongevej",3200,2000,250,1250,3750,10000,14000,18000),
+				new Territory ("Bulowsvej", 3, 2800,2000,200,1000,3000,9000,12500,15000),
+				new Territory ("Gl. Kongevej", 3, 3200,2000,250,1250,3750,10000,14000,18000),
 				new Fleet ("Mols-linien! En genvej i Danmark"),
-				new Territory ("Bernstorffsvej",3600,2000,300,1400,4000,11000,15000,19000),
+				new Territory ("Bernstorffsvej", 4, 3600,2000,300,1400,4000,11000,15000,19000),
 				new Luck ("Prøv lykken"),
-				new Territory ("Hellerupvej",3600,2000,300,1400,4000,11000,15000,19000),
-				new Territory ("Stranvejen",4000,2000,350,1600,4400,12000,16000,20000),
+				new Territory ("Hellerupvej", 4, 3600,2000,300,1400,4000,11000,15000,19000),
+				new Territory ("Stranvejen",4, 4000,2000,350,1600,4400,12000,16000,20000),
 				new Empty ("Parkering"),
-				new Territory ("Trianglen",4400,3000,350,1800,5000,14000,17500,21000),
+				new Territory ("Trianglen",5,4400,3000,350,1800,5000,14000,17500,21000),
 				new Luck ("Prøv lykken"),
-				new Territory ("Østerbrogade",4400,3000,350,1800,5000,14000,17500,21000),
-				new Territory ("Grønningen",4800,3000,400,2000,6000,15000,18500,22000),
+				new Territory ("Østerbrogade",5,4400,3000,350,1800,5000,14000,17500,21000),
+				new Territory ("Grønningen",5,4800,3000,400,2000,6000,15000,18500,22000),
 				new Fleet ("Scanlines! Gedser - Rostock"),
-				new Territory ("Bredgade",5200,3000,450,2200,6600,16000,19500,23000),
-				new Territory ("Kgs. Nytorv",5200,3000,450,2200,6600,16000,19500,23000),
+				new Territory ("Bredgade",6,5200,3000,450,2200,6600,16000,19500,23000),
+				new Territory ("Kgs. Nytorv",6,5200,3000,450,2200,6600,16000,19500,23000),
 				new Labor ("Coca-Cola"),
-				new Territory ("Østerbrogade",5600,3000,500,2400,7200,17000,20500,24000),
+				new Territory ("Østergade",6,5600,3000,500,2400,7200,17000,20500,24000),
 				new Jail ("De fængsles"),
-				new Territory ("Amagertorv",6000,4000,550,2600,7800,18000,22000,25000),
-				new Territory ("Vimmelskaftet",6000,4000,550,2600,7800,18000,22000,25000),
+				new Territory ("Amagertorv",7,6000,4000,550,2600,7800,18000,22000,25000),
+				new Territory ("Vimmelskaftet",7,6000,4000,550,2600,7800,18000,22000,25000),
 				new Luck ("Prøv lykken"),
-				new Territory ("Nygade",6400,4000,600,3000,9000,20000,24000,28000),
+				new Territory ("Nygade",7,6400,4000,600,3000,9000,20000,24000,28000),
 				new Fleet ("Scanlines! Rødby - Puttgarden"),
 				new Luck ("Prøv lykken"),
-				new Territory ("Frederiksberggade",7000,4000,700,3500,10000,22000,26000,30000),
-				new Tax ("Ekstraordinær statsskat - betal",2000, "..Fejl?."),
-				new Territory ("Rådhuspladsen",8000,4000,1000,4000,12000,28000,34000,40000),
+				new Territory ("Frederiksberggade",8,7000,4000,700,3500,10000,22000,26000,30000),
+				new Tax ("Ekstraordinær statsskat - betal",2000,"false"),
+				new Territory ("Rådhuspladsen",8,8000,4000,1000,4000,12000,28000,34000,40000),
 		};
 
+		desktop_fields.Field list[] = new desktop_fields.Field[field.length];
 
 		for (int i = 0; i < field.length; i++) {
 			Field fields = field[i];
@@ -68,8 +76,10 @@ public abstract class Field {
 						.setTitle(field[i].fieldname)
 						.setSubText("Costs: "+field[i].getPrice()+"")
 						.setDescription("Rent: "+field[i].getRent()+"")
+						if (field[i].) {
 						.setBgColor(Color.DARK_GRAY)
 						.setFgColor(Color.WHITE)
+						}
 						.build();
 
 			} else if (fields instanceof Refuge) {
