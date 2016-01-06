386c7f2ae3dcbe1e837c870df7ca6a048a1e0400
Sebastian <sebastian@vestrup.net>
2016-01-06 15:54:06 +0100

    Flyttet luck-metoden til game (rettet mindre fejl ved sidste commit).

--- 24_CDIO_Final/src/fields/Luck.java
+++ 24_CDIO_Final/src/fields/Luck.java
@@ -6,74 +6,12 @@ import cards.CardsMoveto;
 import game.Player;
 
 public class Luck extends Field {
-	
-	private CardsDeck deck;
-	
+		
     public Luck(String name) {
         super(name);
     }
 
 	
-	public void landOnField(Player player) {
-		Cards card= deck.drawcard();
-		if (card instanceof CardsMoveto) {
-            CardsMoveto move=(CardsMoveto) card;
-            if (move.getExtraMoves()==0){
-            	
-            }
-            	
-            
-            }
-            //		switch(player.getPlayerPosition())
-//		case 1:
-//		case 2:
-//		case 3:
-//		case 4:
-		if(player.getPlayerPosition()>0 && player.getPlayerPosition()<10)
-		{
-		player.setPlayerPosistion(player, 6);
-		Fleet ships = (Fleet)fields[6];
-			if(ships.fieldowned)
-			{
-				player.setPlayerPosistion(player, 6);
-				ships.landOnField(player);
-			}
-			else
-				player.setPlayerPosistion(player, 6);
-				ships.landOnField(player);
-		}
-		else if(player.getPlayerPosition()>=11 && player.getPlayerPosition()<=20)
-		{
-		player.setPlayerPosistion(player, 16);
-		Fleet ships = (Fleet)fields[16];
-			if(ships.fieldowned)
-			{
-			player.setPlayerPosistion(player, 16);
-			ships.landOnField(player);
-			}
-		}
-		else if(player.getPlayerPosition()>=21 && player.getPlayerPosition()<=30)
-		{
-		player.setPlayerPosistion(player, 26);
-		Fleet ships = (Fleet)fields[26];
-			if(ships.fieldowned)
-			{
-			player.setPlayerPosistion(player, 26);
-			ships.landOnField(player);
-			}
-		}
-		else if(player.getPlayerPosition()>=31 && player.getPlayerPosition()<=40)
-		{
-		player.setPlayerPosistion(player, 36);
-		Fleet ships = (Fleet)fields[36];
-			if(ships.fieldowned)
-			{
-			player.setPlayerPosistion(player, 36);
-			ships.landOnField(player);
-			}
-		}
-		
-            
-	}
+	
     
 }
--- 24_CDIO_Final/src/game/Game.java
+++ 24_CDIO_Final/src/game/Game.java
@@ -6,6 +6,9 @@
 
 package game;
 
+import cards.Cards;
+import cards.CardsDeck;
+import cards.CardsMoveto;
 import desktop_resources.GUI;
 import fields.Empty;
 import fields.Field;
@@ -18,8 +21,9 @@ import fields.Refuge;
 import fields.Territory;
 import setup.Setup;
 
-public class Game {
 
+public class Game {
+	private CardsDeck deck;
 	private static Player[] player;
 	protected static Field[] fields;
 
@@ -29,6 +33,8 @@ public class Game {
 
 	public Game() {
 
+		deck = new CardsDeck();
+		
 		// Create Fields
 		fields = Field.createFields();
 		
@@ -116,6 +122,73 @@ public class Game {
 	private void landOnLuck(Player player, Field currentfield) {
 		
 		// Draw a luck card
+		
+
+			Cards card = deck.drawcard();
+			if (card instanceof CardsMoveto) {
+	            CardsMoveto move=(CardsMoveto) card;
+	            if (move.getExtraMoves()==0){
+	            	
+	            }
+	            	
+	            
+	            }
+
+			if(player.getPlayerPosition()>0 && player.getPlayerPosition()<10)
+			{
+			player.setPlayerPosistion(player, 6);
+			Fleet ships = (Fleet)fields[6];
+				if(ships.fieldowned)
+				{
+					player.setPlayerPosistion(player, 6);
+					landOnOwnable(player,fields[6]);
+				}
+				else
+					player.setPlayerPosistion(player, 6);
+					landOnOwnable(player,fields[6]);
+			}
+			else if(player.getPlayerPosition()>=11 && player.getPlayerPosition()<=20)
+			{
+			player.setPlayerPosistion(player, 16);
+			Fleet ships = (Fleet)fields[16];
+				if(ships.fieldowned)
+				{
+				player.setPlayerPosistion(player, 16);
+				landOnOwnable(player,fields[16]);
+				}
+				else
+					player.setPlayerPosistion(player, 16);
+					landOnOwnable(player,fields[16]);
+			}
+			else if(player.getPlayerPosition()>=21 && player.getPlayerPosition()<=30)
+			{
+			player.setPlayerPosistion(player, 26);
+			Fleet ships = (Fleet)fields[26];
+				if(ships.fieldowned)
+				{
+				player.setPlayerPosistion(player, 26);
+				landOnOwnable(player,fields[26]);
+				}
+				else
+					player.setPlayerPosistion(player, 26);
+					landOnOwnable(player,fields[26]);
+			}
+			else if(player.getPlayerPosition()>=31 && player.getPlayerPosition()<=40)
+			{
+			player.setPlayerPosistion(player, 36);
+			Fleet ships = (Fleet)fields[36];
+				if(ships.fieldowned)
+				{
+				player.setPlayerPosistion(player, 36);
+				landOnOwnable(player,fields[36]);
+				}
+				else
+					player.setPlayerPosistion(player, 36);
+					landOnOwnable(player,fields[36]);
+			}
+			
+	            
+		
 		
 	}
 
