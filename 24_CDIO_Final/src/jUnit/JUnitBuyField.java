cc3945a50850e2c89bb7d169388564ba54597c4d
Nicki N <nickinylin@hotmail.com>
2016-01-08 14:27:19 +0100

    Opdateret Junit test

--- 24_CDIO_Final/src/jUnit/JUnitBuyField.java
+++ 24_CDIO_Final/src/jUnit/JUnitBuyField.java
@@ -10,34 +10,58 @@ import controllers.PlayerController;
 import controllers.TerritoryController;
 import desktop_resources.GUI;
 import fields.Field;
+import fields.Territory;
 import game.Player;
 import setup.Setup;
 
 public class JUnitBuyField {
-	
+
 	private Player spiller1;
-    private Player spiller2;
-    
+	private Player spiller2;
+
 	private Player[] players;
 	protected Field[] fields;
 	TerritoryController t = new TerritoryController();
-	
-    @Before
-    public void setUp(){
+
+	@Before
+	public void setUp(){
 		Setup setup = new Setup();
 		// Create Fields players and card deck
 		fields = setup.createFields();
-		
-		
-        this.spiller1= new Player("Lars");
-        this.spiller2= new Player("Svend");
-    }
-		
+
+		this.spiller1= new Player("Lars");
+		this.spiller2= new Player("Svend");
+		spiller1.setPlayerPosition(1);
+	}
+
+	@Test
+	public void testSetFieldOwned() {
+		Field currentfield = fields[spiller1.getPlayerPosition()-1];
+		((Territory) currentfield).buyField(spiller1, fields);
+		//		t.landOnTerritory(players, spiller1,(Territory) currentfield,fields);
+
+		assertEquals(true, ((Territory) currentfield).fieldowned);
+	} 
+
 	@Test
 	public void testSetFieldOwner() {
-		t.landOnTerritory(players, spiller1, fields[1], fields);
-		
-		
+		Field currentfield = fields[spiller1.getPlayerPosition()-1];
+		((Territory) currentfield).buyField(spiller1, fields);
+
+		assertEquals(spiller1, ((Territory) currentfield).fieldowner);
+	}
+
+	@Test //tester om huslejen bliver fordoblet ved at eje alle felter i samme gruppe
+	public void testBuyFieldGroup() {
+
+		Field currentfield = fields[spiller1.getPlayerPosition()-1];
+		((Territory) currentfield).buyField(spiller1, fields);
+
+		spiller1.setPlayerPositionToField(4);
+		currentfield = fields[spiller1.getPlayerPosition()-1];
+		((Territory) currentfield).buyField(spiller1, fields);
+
+		assertEquals(100, ((Territory) currentfield).getRent(spiller1, fields) );
 	}
 
 }
