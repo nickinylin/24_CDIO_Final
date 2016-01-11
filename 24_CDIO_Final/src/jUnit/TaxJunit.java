0e2b0ff9703f93ef6761fd7fe87d30f6ff97c850
Mathias Kvisgaard <mathias@kvisgaard.info>
2016-01-11 14:05:21 +0100

    Tax test er næsten sat op. JailJunittest2 er fjernet da den ikke virkede

--- 24_CDIO_Final/src/jUnit/TaxJunit.java
+++ 24_CDIO_Final/src/jUnit/TaxJunit.java
@@ -2,13 +2,35 @@ package jUnit;
 
 import static org.junit.Assert.*;
 
+import controllers.*;
+import game.*;
+import setup.*;
+import fields.*;
+import cards.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class TaxJunit {
 
+	private Player player1;
+	private Bank b;
+	private TaxController t;
+	private Field f;
+	
+	@Before
+	public void setUp(){
+		player1 = new Player("Hans");
+		b = new Bank();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void pay10percent() {
+		player1.setAssets(30000);
+//		t.payTax(player1, payamount);
+		
+			
 	}
 
 }
