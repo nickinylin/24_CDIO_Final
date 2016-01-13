e71c24b9cf2fd0c78d9e9e0e43b2d4c3d26d6702
Kasper Emil Okkels <kasperemil38@gmail.com>
2016-01-13 14:15:51 +0100

    Spillere tvinges nu til at gå bankerot og sælge samtlige felter hvis de får negative assets.

--- 24_CDIO_Final/src/controllers/Game.java
+++ 24_CDIO_Final/src/controllers/Game.java
@@ -167,6 +167,7 @@ public class Game {
 		for (int i = 1; i < players.length; i++) {
 
 			if (players[i].bankruptCheck()) {
+				menuController.sellAllFields(players[i], fields);
 				count++;
 			}
 
