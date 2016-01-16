5e1a5dc83254024e57d912f6e466deee0d4537eb
pc <pc@192.168.1.39>
2016-01-16 17:16:40 +0100

    JUnit Bankrupt test

---
+++ 24_CDIO_Final/src/jUnit/JUnitBankrupt.java
@@ -0,0 +1,35 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import fields.Field;
+import game.Player;
+import setup.Setup;
+
+public class JUnitBankrupt {
+
+	private Player spiller1;
+	private Player spiller2;
+	
+	@Before
+	public void setUp()	{
+		Setup setup = new Setup();
+		setup.createFields();
+		
+		this.spiller1 = new Player("Lars");
+		this.spiller2 = new Player("Svend");
+		spiller1.setPlayerPosition(1);
+		spiller2.setPlayerPosition(1);
+		
+	}
+	
+	@Test
+	public void testBankrupt() {
+		
+		spiller1.setBankrupt();
+		
+		assertEquals(true, spiller1.getBankrupt());
+	}
+	
+}
